d3e6e444b8ded7dd4380e6bd84d8d77ac341029b
Kittycarr <taya.khachko@gmail.com>
2025-06-24 22:11:15 +0200

    sfdajkgfwdjbkdHWFKegkrgiznrzhkaeromihegyrzgurin8zeqMUqeghguznqweoasfhilsdhfbjfdskbjdasfdsfjksdhjds

--- src/main/java/btw/community/example/KittysFirstAddon.java
+++ src/main/java/btw/community/example/KittysFirstAddon.java
@@ -2,16 +2,20 @@ package btw.community.example;
 
 import btw.AddonHandler;
 import btw.BTWAddon;
+import btw.community.kittystesting.items.KittysItems;
 
-public class ExampleAddon extends BTWAddon {
-    private static ExampleAddon instance;
+public class KittysFirstAddon extends BTWAddon {
+    private static KittysFirstAddon instance;
 
-    public ExampleAddon() {
+    public KittysFirstAddon() {
         super();
     }
 
     @Override
     public void initialize() {
         AddonHandler.logMessage(this.getName() + " Version " + this.getVersionString() + " Initializing...");
+
+        KittysItems.InitializeItems();
+
     }
 }
--- src/main/java/btw/community/kittystesting/items/KittysItems.java
+++ src/main/java/btw/community/kittystesting/items/KittysItems.java
@@ -3,11 +3,16 @@ package btw.community.kittystesting.items;
 import btw.item.items.FoodItem;
 import net.minecraft.src.CreativeTabs;
 
+
+
 public class KittysItems {
 
     public static FoodItem chickenBurger;
 
-    static {
+    public static void InitializeItems() {
         chickenBurger = (FoodItem) new FoodItem(69000, 6, 1f, false, "kittyschickenburger", true ).setCreativeTab(CreativeTabs.tabFood).setTextureName("kittyschickenburger").setMaxStackSize(16);
+
     }
+
 }
+
